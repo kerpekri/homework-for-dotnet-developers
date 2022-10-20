e5a8d1c19f69ecf757d32f0608b729e315b9e522
Vilis Trautmanis <pm2u@eurofins.com>
2022-10-20 13:07:01 +0300

    Adding AreaCalculator.

---
+++ developer-interview/SolidPrinciples/SolidPrinciples/AreaCalculator.cs
@@ -0,0 +1,31 @@
+﻿namespace SolidPrinciples;
+
+public class AreaCalculator
+{
+    public double GetArea(IShape shape)
+    {
+        if (shape is Square square)
+        {
+            return square.Length * 4;
+        }
+        else
+        {
+            return Math.PI * Math.Pow(((Circle)shape).Radius, 2);
+        }
+        //TODO: What if we would want to add triangle area calculation?
+    }
+}
+
+public interface IShape
+{
+}
+
+public class Square
+{
+    public int Length { get; set; }
+}
+
+public class Circle
+{
+    public int Radius { get; set; }
+}
