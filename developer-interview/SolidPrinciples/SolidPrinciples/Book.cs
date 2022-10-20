551291cd143ad6af7d1fda5feaca633f5a499f1a
Vilis Trautmanis <pm2u@eurofins.com>
2022-10-20 12:46:53 +0300

    Adding developer interview project.

---
+++ developer-interview/SolidPrinciples/SolidPrinciples/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace SolidPrinciples;
+
+public class Book {
+
+    private String name;
+    private String author;
+    private String text;
+
+    public String replaceWordInText(string word, String replacementWord){
+        return text.Replace(word, replacementWord);
+    }
+
+    public bool isWordInText(string word){
+        return text.Contains(word);
+    }
+
+    public void PrintTextToConsole()
+    {
+        Console.Write(text);
+    }
+}
